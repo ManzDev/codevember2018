32d26074e6b1356a20d1948eaef5e75ef67d37f3
ManzDev <JoseRomanHernandez@gmail.com>
2018-11-07 18:41:53 +0000

    Fix paths (windows scripts error)

--- .bin/build.sh
+++ .bin/build.sh
@@ -2,5 +2,5 @@ if [ "$1" == "" ]; then
   echo Syntax: npm run build [day number]
 else
   rm -rf day-$1/*
-  parcel build src/day-$1/index.html -d day-$1 --global global --public-url /codevember2018/day-$1/
+  parcel build src/day-$1/index.html -d day-$1 --no-source-maps --global global --public-url https://manzdev.github.io/codevember2018/day-$1/
 fi
